99bb99f9b1886bba307e24132ac9099f6de8d4fe
JasurJorayev <jorayevjr@gmil.com>
2026-04-16 14:54:12 +0500

    Reverse the numbers

---
+++ main.js
@@ -0,0 +1,53 @@
+function num(son){
+
+    if(son ==0)  return "0 dan katta son kiriting"
+    if(son < 9) return son
+
+if(son<99){
+    if(son % 10 ==0){
+        let birlik = son % 10
+        let onlik = son % 100 / 10
+        let natija=(birlik.toString())+(onlik.toString())
+        return natija
+    }else{
+        let birlik = son % 10
+        let onlik = son % 100 / 10
+
+        return (birlik*10)+(Math.trunc(onlik))
+    }
+}else if(son<999){
+    if(son % 10 == 0){
+        let birlik = son % 10
+        let onlik = Math.trunc(son % 100 / 10)
+        let yuzlik = Math.trunc(son / 100 % 10)
+
+        let natija = (birlik.toString()  +   (Math.trunc(onlik.toString()) ) +  yuzlik.toString())
+        return natija
+    }else{
+        let birlik = son % 10
+        let onlik = Math.trunc(son % 100 / 10)
+        let yuzlik = Math.trunc(son / 100 )
+
+        return  (birlik*100)  +  (onlik*10)  +  yuzlik
+    }
+}else if(son<9999){
+    if(son % 10 ==0){
+        let birlik = son % 10
+        let onlik = Math.trunc(son % 100 / 10)
+        let yuzlik = Math.trunc(son / 100 % 10)
+        let minglik = Math.trunc(son / 1000 % 100)
+
+        let natija = (birlik.toString()  +   (Math.trunc(onlik.toString()) ) +  yuzlik.toString() + minglik.toString())
+        return natija
+    }else{
+        let birlik = son % 10
+        let onlik = Math.trunc(son % 100 / 10)
+        let yuzlik = Math.trunc(son / 100 % 10)
+        let minglik = Math.trunc(son / 1000 % 100)
+
+        return (birlik*1000)  +  (onlik*100)  +  (yuzlik*10) + minglik
+    }
+  }
+}
+let result = num(7)
+console.log(result);
